76bce30d92fcab81ef8ee35c385b5c45347cd957
ployploy <chonnikan.t@kkumail.com>
2018-06-28 17:04:14 +0700

    [Added]:  เพิ่มการเรียกใช้  struct จาก package handler

--- src/mysql/main.go
+++ src/mysql/main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"log"
-	"mysql/service"
+	"mysql/handler"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/create", service.CreateMessageHandler)
-	http.HandleFunc("/delete", service.Delete)
-	http.HandleFunc("/update", service.Update)
-	http.HandleFunc("/readbyid", service.ReadById)
-	http.HandleFunc("/read", service.Read)
+	api := handler.Api{
+		DBDriver:   "mysql",
+		DBUsername: "root",
+		DBPassword: "sckshuhari",
+		DBName:     "sckseal",
+	}
+	http.HandleFunc("/create", api.CreateMessageHandler)
+	http.HandleFunc("/delete", api.Delete)
+	http.HandleFunc("/update", api.Update)
+	http.HandleFunc("/readbyid", api.ReadById)
+	http.HandleFunc("/read", api.Read)
 
 	log.Println("Listening...")
 	http.ListenAndServe(":3000", nil)
