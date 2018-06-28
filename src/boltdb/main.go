e7d20fc8a27ae2e6e2aa85435cfd0d26841dd0d4
platylm <n_nareenart@kkumail.com>
2018-06-28 17:10:30 +0700

    [Edit]:crud.go

--- src/boltdb/main.go
+++ src/boltdb/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"boltdb/crud"
+	"encoding/json"
 	"log"
 	"net/http"
 
@@ -11,24 +12,35 @@ import (
 func main() {
 	type Person struct {
 		Name string
-		Age  int
+		Age  string
 	}
 
 	// Open the database.
-	db, err := bolt.Open("src/boltdb/test.db", 0666, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	http.HandleFunc("/setperson", func(w http.ResponseWriter, r http.Request){
-		u := r.URL.Query()
-		crud.Insert_DB(db)
-	}
-	crud.View_DB(db)
+	db, _ := bolt.Open("src/boltdb/test.db", 0666, nil)
 
+	http.HandleFunc("/setperson", func(w http.ResponseWriter, r *http.Request) {
+		crud.Insert_DB(db)
+	})
+
+	http.HandleFunc("/getperson", func(w http.ResponseWriter, r *http.Request) {
+
+		// Read the value back from a separate read-only transaction.
+		db.View(func(tx *bolt.Tx) error {
+			widgets := tx.Bucket([]byte("widgets"))
+			person := Person{
+				Name: string(widgets.Get([]byte("Name"))),
+				Age:  string(widgets.Get([]byte("Age"))),
+			}
+			json.NewEncoder(w).Encode(&person)
+			return nil
+		})
+	})
 	// Close database to release the file lock.
 	if err := db.Close(); err != nil {
 		log.Fatal(err)
 	}
-	
-	http.ListenAndServe("localhost:8000",nil)
+
+	http.ListenAndServe("localhost:8000", nil)
 }
+
+//sfunc Read(db *bolt.DB) ht
