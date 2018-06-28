d764355d5e258aa5b6ee321059526a4b246fd348
ployploy <chonnikan.t@kkumail.com>
2018-06-28 16:07:14 +0700

    [Added]: เพิ่ม routing /update and /delete

--- src/mysql/main.go
+++ src/mysql/main.go
@@ -8,6 +8,8 @@ import (
 
 func main() {
 	http.HandleFunc("/create", service.CreateMessageHandler)
+	http.HandleFunc("/delete", service.Delete)
+	http.HandleFunc("/update", service.Update)
 
 	log.Println("Listening...")
 	http.ListenAndServe(":3000", nil)
