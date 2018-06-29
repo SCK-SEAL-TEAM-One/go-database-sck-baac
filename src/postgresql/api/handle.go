0e2765cc38a9341be61d9d77ad4323f86aaf18fb
golfapipol <golf.apipol@gmail.com>
2018-06-29 11:50:07 +0700

    [Add]: api, crud, main, model of postgresql

--- src/postgresql/api/handle.go
+++ src/postgresql/api/handle.go
@@ -1,1 +1,57 @@
 package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"postgresql/crud"
+)
+
+func Read() http.HandlerFunc {
+
+	sayhi := crud.ReadSayhi()
+
+	return func(responseWriter http.ResponseWriter, request *http.Request) {
+		JSONConnectData, _ := json.Marshal(sayhi)
+		responseWriter.Write(JSONConnectData)
+	}
+}
+
+func ReadById() http.HandlerFunc {
+
+	sayhi := crud.ReadSayhi()
+
+	return func(responseWriter http.ResponseWriter, request *http.Request) {
+		JSONConnectData, _ := json.Marshal(sayhi)
+		responseWriter.Write(JSONConnectData)
+	}
+}
+
+func Add() http.HandlerFunc {
+
+	sayhi := crud.ReadSayhi()
+
+	return func(responseWriter http.ResponseWriter, request *http.Request) {
+		JSONConnectData, _ := json.Marshal(sayhi)
+		responseWriter.Write(JSONConnectData)
+	}
+}
+
+func Edit() http.HandlerFunc {
+
+	sayhi := crud.ReadSayhi()
+
+	return func(responseWriter http.ResponseWriter, request *http.Request) {
+		JSONConnectData, _ := json.Marshal(sayhi)
+		responseWriter.Write(JSONConnectData)
+	}
+}
+
+func Delete() http.HandlerFunc {
+
+	sayhi := crud.ReadSayhi()
+
+	return func(responseWriter http.ResponseWriter, request *http.Request) {
+		JSONConnectData, _ := json.Marshal(sayhi)
+		responseWriter.Write(JSONConnectData)
+	}
+}
--- src/postgresql/crud/crud.go
+++ src/postgresql/crud/crud.go
@@ -1,1 +1,50 @@
 package crud
+
+import (
+	"database/sql"
+	"fmt"
+	"postgresql/model"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "1234"
+	dbname   = "sckseal"
+)
+
+func ConnectDB() (db *sql.DB) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func ReadSayhi(id string, postgresURL string) model.Sayhi {
+	db := ConnectDB()
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM sayhi")
+	var listsayhi [10]model.Sayhi
+	for rows.Next() {
+		var id int
+		var description string
+		err = rows.Scan(&id, &description)
+	}
+	return listsayhi
+}
+
+// func ReadSayhiByID() string {
+// 	return "ggggg"
+// }
+
+// func ReadSayhi() string {
+// 	return "ggggg"
+// }
