c07dbd50986d23cd5e96634f41c7ca683b2fecea
Apipol Sukgler <apipol@computerlogy.com>
2018-06-29 10:44:14 +0700

    [Added]: เขียนเทส Test_HelloWorldReadById_Input_id_hello_world_Should_Be_hello_world สำหรับเทส ฟังก์ชั่น GetReadById และเขียนฟังก์ชันให้สามารถใช้งานได้

--- src/redis/api/helloWorldHandler.go
+++ src/redis/api/helloWorldHandler.go
@@ -18,3 +18,19 @@ func GetReadAll(ReadAllKeyFunc func() ([]map[string]string, error)) func(http.Re
 
 	}
 }
+
+func GetReadById(ReadByIdFunc func(string) (map[string]string, error)) func(http.ResponseWriter, *http.Request) {
+	return func(responseWriter http.ResponseWriter, request *http.Request) {
+		queryString := request.URL.Query()
+		id := queryString.Get("id")
+		list, err := ReadByIdFunc(id)
+		if err != nil {
+			http.Error(responseWriter, err.Error(), 500)
+			return
+
+		}
+		jsonResponse, _ := json.Marshal(list)
+		responseWriter.Write(jsonResponse)
+
+	}
+}
--- src/redis/api/helloWorldHandler_test.go
+++ src/redis/api/helloWorldHandler_test.go
@@ -29,3 +29,25 @@ func Test_HelloWorldReadAll_Should_Be_Get_All_Keys(t *testing.T) {
 		t.Errorf("Should Be %s but it got %s", expected, actual)
 	}
 }
+
+func Test_HelloWorldReadById_Input_id_hello_world_Should_Be_hello_world(t *testing.T) {
+	stubReadByIdFunc := func(string) (map[string]string, error) {
+		return map[string]string{
+			"id":          "hello world",
+			"description": "hello world",
+		}, nil
+	}
+	readById := GetReadById(stubReadByIdFunc)
+	url := `/helloworld/readById?id=hello%20world`
+	request := httptest.NewRequest("GET", url, nil)
+	responseRecorder := httptest.NewRecorder()
+	expected := `{"description":"hello world","id":"hello world"}`
+
+	readById(responseRecorder, request)
+	response := responseRecorder.Result()
+	actual, _ := ioutil.ReadAll(response.Body)
+
+	if string(actual) != expected {
+		t.Errorf("Should Be %s but it got %s", expected, actual)
+	}
+}
